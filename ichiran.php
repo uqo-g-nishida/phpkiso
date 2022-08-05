3c577381d5eb602412518321e693033c51138cc2
uqo-g-nishida <g.nishida@uqo.jp>
2022-08-05 10:20:34 +0900

    update kensaku

--- ichiran.php
+++ ichiran.php
@@ -5,9 +5,41 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>一覧</title>
 </head>
 <body>
+
+<?php
+$code = $_POST['code'];
+
+// DB接続
+$dsn = 'mysql:dbname=phpkiso;host=localhost';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->query('SET NAMES utf8');
+
+$sql = 'SELECT * FROM anketo WHERE code=?';
+$stmt = $dbh->prepare($sql);
+$data[]=$code;
+$stmt->execute($data);
+
+while (1) {
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($rec == false) {
+        break;
+    }
+    echo "
+        ${rec['code']}
+        ${rec['nickname']}
+        ${rec['email']}
+        ${rec['goiken']}
+        <br>
+        ";
+}
+
+$dbh = null;
+?>
 
 </body>
 </html>
--- thanks.php
+++ thanks.php
@@ -5,43 +5,48 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
+    <title>Thanks</title>
 </head>
 <body>
 
 <?php
-// DB接続
-$dsn = 'mysql:dbname=phpkiso;host=localhost';
-$user = 'root';
-$password = '';
-$dbh = new PDO($dsn, $user, $password);
-$dbh->query('SET NAMES utf8');
-
 $nickname=$_POST['nickname'];
 $email=$_POST['email'];
 $goiken=$_POST['goiken'];
 
-echo "
+try {
+    // DB接続
+    $dsn = 'mysql:dbname=phpkiso;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+
+    echo "
     $nickname 様<br>
     ご意見ありがとうございました。<br>
     いただいたご意見「$goiken 」<br>
     $email 宛てにメールを送りましたのでご確認ください。
     ";
 
-$mail_sub='アンケートを受け付けました。';
-$mail_body="$nickname 様へ¥nアンケートご協力ありがとうございました。";
-$mail_body=html_entity_decode($mail_body, ENT_QUOTES, "UTF-8");
-$mail_head='From: xxx@xxx.co.jp';
-mb_language('Japanese');
-mb_internal_encoding("UTF-8");
-mb_send_mail($email, $mail_sub, $mail_body, $mail_head);
+    $mail_sub='アンケートを受け付けました。';
+    $mail_body="$nickname 様へ¥nアンケートご協力ありがとうございました。";
+    $mail_body=html_entity_decode($mail_body, ENT_QUOTES, "UTF-8");
+    $mail_head='From: xxx@xxx.co.jp';
+    mb_language('Japanese');
+    mb_internal_encoding("UTF-8");
+    mb_send_mail($email, $mail_sub, $mail_body, $mail_head);
 
-// DBに自動保存
-$sql = 'INSERT INTO anketo (nickname,email,goiken) VALUES ("'.$nickname.'","'.$email.'","'.$goiken.'")';
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
+    // DBに自動保存
+    $sql = 'INSERT INTO anketo (nickname,email,goiken) VALUES ("'.$nickname.'","'.$email.'","'.$goiken.'")';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
 
-$dbh = null;
+    $dbh = null;
+} catch (Exception $e) {
+    echo 'ただいま障害により大変ご迷惑をおかけしております。';
+}
+
 ?>
 
 </body>
